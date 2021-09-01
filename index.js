88f189643a35100f19bdac7717e7901b9f16b40d
mgaitan <mgaitan@bytesw.com>
2021-09-01 10:54:53 -0600

    Card con react-native-elements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from "react-native";
 import React from "react";
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from "react-native";
-import { Card } from "react-native-paper";
+import { Card } from 'react-native-elements'
 
 const DATA = [
     {
@@ -31,13 +31,16 @@ const DATA = [
 ]
 
 const Item = ({ title, img, description }) => (
-    <Card style={styles.item}>
+    <Card containerStyle={styles.item}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Divider color="black"/>
         <View style={{flexDirection: 'row'}}>
             <View>
-                <Image style={styles.img} source={img} />
+                <Card containerStyle={{borderRadius:100, backgroundColor:'#BCC8DC', borderWidth: 0}}>
+                    <Card.Image style={styles.img} source={img}></Card.Image>
+                </Card>
             </View>
-            <View>
-                <Text style={styles.title}>{title}</Text>
+            <View style={{width: 200}}>
                 <Text style={styles.desc}>{description}</Text>
             </View>
         </View>
@@ -75,11 +78,11 @@ const styles = StyleSheet.create({
         paddingLeft: 5
     },
     title: {
-        fontSize: 32,
-        marginLeft: 10
+        fontSize: 32
     },
     desc: {
-        marginLeft: 10,
+        textAlign:'justify',
+        marginTop: 50
     },
     img:{
         width: 100,
